f165e192486d40fd95329dcc143d5a47b53cf269
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-10-20 16:09:13 -0700

    Fix the CORS error

--- backend/app.ts
+++ backend/app.ts
@@ -8,6 +8,11 @@ type Express = express.Application;
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 
 pool
     .connect()
