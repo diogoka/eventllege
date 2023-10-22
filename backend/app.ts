f7fec13fd898ca048c6639a54042bc8bbd7febbd
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-10-22 14:31:38 -0700

    fixed route problem

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,8 @@
-
-import express from 'express';
-import pool from './db/db';
-import usersRouter from './routes/usersRoutes';
-import coursesRouter from './routes/coursesRoutes';
-import eventsRoutes from './routes/eventsRoutes';
-
+import express from "express";
+import pool from "./db/db";
+import usersRouter from "./routes/usersRoutes";
+import coursesRouter from "./routes/coursesRoutes";
+import eventsRouter from "./routes/eventsRoutes";
 
 type Express = express.Application;
 
@@ -21,7 +19,6 @@ pool
   .connect()
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log("Error connecting to database", err));
-
 
 app.use("/api/users", usersRouter);
 app.use("/api/courses", coursesRouter);
