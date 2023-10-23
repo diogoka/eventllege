343022d4ea5745b3f2802c92b28acfc29f6ee108
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-10-22 19:30:21 -0700

    made update events

--- backend/routes/eventsRoutes.ts
+++ backend/routes/eventsRoutes.ts
@@ -1,11 +1,13 @@
 import express, { Router } from "express";
-import { createEvents, getEvents, deleteEvents, newAttendee } from "../controllers/eventsControllers";
+import { createEvents, getEvents, updateEvents, deleteEvents, newAttendee } from "../controllers/eventsControllers";
 
 const eventsRouter: Router = express.Router();
 
 eventsRouter.get("/", getEvents);
 
 eventsRouter.post("/new", createEvents);
+
+eventsRouter.put("/:id", updateEvents);
 
 eventsRouter.delete("/:id", deleteEvents);
 
