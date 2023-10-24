2a682f0fffa6782e8a33a58a2529bd097a7d9f8d
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-10-23 23:52:30 -0700

    Create custom hook for image upload

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react"
 import { Stack } from "@mui/material";
 import axios from "axios";
+import useUploadImage from "@/services/imageInput";
 
 // For now, display info of the user whose ID is A
 // Use ID of the logged-in user later
@@ -10,15 +11,36 @@ const SAMPLE_USER_ID = 'A';
 type User = {
   id: string;
   role: string;
+  course: string;
   name: string;
   email: string;
   postalCode: string;
   phone: string;
 }
 
+type Course = {
+  id: number;
+  name: string;
+  category: string;
+}
+
+
 export default function UserPage() {
 
   const [user, setUser] = useState<User>();
+  const [userInEdit, setUserInEdit] = useState<User>();
+  const [isEditting, setIsEditing] = useState(false);
+
+  // User Input
+  const [courseId, setCourseId] = useState(1);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [phone, setPhone] = useState("");
+  const { image, warning, onFileInputChange } = useUploadImage(10, 0.1, 480);
+
+  // Course data from server
+  const [courses, setCourses] = useState([]);
 
   // Get course data from server to show course names
   useEffect(() => {
@@ -32,16 +54,79 @@ export default function UserPage() {
       })
   }, []);
 
+  // Get course data from server to show course names
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/api/courses')
+      .then((res) => {
+        setCourses(res.data);
+      })
+      .catch((error) => {
+        console.error(error.response.data);
+      })
+  }, []);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("id", SAMPLE_USER_ID);
+    formData.append("type", "2");
+    formData.append("courseId", courseId.toString());
+    formData.append("email", email);
+    formData.append("name", name);
+    if (postalCode) formData.append("postalCode", postalCode);
+    if (phone) formData.append("phone", phone);
+    if (image) formData.append("avatar", image);
+
+    axios
+      .put('http://localhost:3001/api/users', formData, {
+        headers: { 'content-type': 'multipart/form-data' }
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error.response.data);
+      })
+  };
+
   return (
     <Stack width={1 / 3}>
-      <h1>User Page</h1>
-      <div>{user?.id}</div>
-      <div>{user?.name}</div>
-      <div>{user?.role}</div>
-      <div>{user?.email}</div>
-      <div>{user?.postalCode}</div>
-      <div>{user?.phone}</div>
-      <img src={`http://localhost:3001/img/users/${user?.id}`} width={'30px'}/>
+      {isEditting ? (
+        <form onSubmit={handleSubmit}>
+
+          <select onChange={(e) => { setCourseId(Number(e.target.value)) }}>
+            {courses.map((course: Course, index: number) => {
+              return (
+                <option key={index} value={course.id}>{course.name}</option>
+              )
+            })}
+          </select>
+
+          <input type="text" placeholder="email" onChange={(event) => setEmail(event.target.value)} />
+          <input type="text" placeholder="name" onChange={(event) => setName(event.target.value)} />
+          <input type="text" placeholder="postal code" onChange={(event) => setPostalCode(event.target.value)} />
+          <input type="text" placeholder="phone" onChange={(event) => setPhone(event.target.value)} />
+          <input type="file" accept="image/*" onChange={onFileInputChange} />
+          <div>{warning}</div>
+
+          <input type="submit" value="Register" />
+        </form>
+      ) : (
+        <>
+          <h1>User Page</h1>
+          <div>{user?.id}</div>
+          <div>{user?.name}</div>
+          <div>{user?.role}</div>
+          <div>{user?.email}</div>
+          <div>{user?.postalCode}</div>
+          <div>{user?.phone}</div>
+          <img src={`http://localhost:3001/img/users/${user?.id}`} width={'30px'} />
+
+          <button style={{ width: 50 }} onClick={() => setIsEditing(true)}>Edit</button>
+        </>
+      )}
     </Stack>
   )
 }
