4f9c14e4187e1e30101fbcd5ea30660c96ee061c
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-10-26 13:25:34 -0700

    Send email about upcoming events

---
+++ backend/automation/notifyNewEvents.ts
@@ -0,0 +1,38 @@
+import { sendEmail, EmailOption } from "../helpers/mail";
+import pool from "../db/db";
+
+try {
+  await pool.connect();
+
+  const emailsResult = await pool.query('SELECT email_user FROM users');
+  const emails: string[] = emailsResult.rows.map((row: any) => {
+    return row.email_user;
+  })
+
+  const eventsResult = await pool.query('SELECT * FROM events');
+  const eventDetails = eventsResult.rows.map((row: any) => {
+    return `
+      Date: ${row.date_event_start.toDateString()}
+      Event Name: ${row.name_event}
+    `;
+  })
+  
+  const option: EmailOption = {
+    to: emails,
+    subject: `Unveiling Our Events!`,
+    text: eventDetails.join('\n')
+  }
+  sendEmail(option, (error: any, info: any) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+    process.exit(0);
+  });
+  
+} catch (error: any) {
+  console.error(error);
+
+  process.exit(1);
+}
