375db1f65300927dcd330dbd432c9d3821b8910d
Daisuke <skd.itkn@gmail.com>
2023-10-27 16:45:21 -0700

    issue 17 done

--- frontend/src/app/history/page.tsx
+++ frontend/src/app/history/page.tsx
@@ -1,10 +1,10 @@
-"use client";
-import { useEffect,useState } from "react"
+'use client'
+import React, { useState, useEffect } from 'react'
 import { Container, Stack, Typography } from "@mui/material"
 import axios from "axios"
 
-export default function EventsPage() {
-
+export default function PastEvent() {
+  
   type Event={
     capacity_event: number,
     category_event: string,
@@ -24,61 +24,24 @@ export default function EventsPage() {
     name_tag: string
   }
 
-  const [events, setEvents]= useState<Array<Event>>([]);
-  const [tags, setTags]= useState<Array<Tag>>([]);
-
-  const[user, setUser]= useState({
-    id_user: "F",
-    name: "John",
-    surname: "Doe",
-  });
-
+  const [events, setEvents] = useState<Array<Event>>([]);
+  const [tags, setTags] = useState<Array<Tag>>([]);
+  
   useEffect(() => {
     
-    axios.get("http://localhost:3001/api/events")
+    axios.get("http://localhost:3001/api/events/")
     .then((res) => {
       console.log("res",res.data)
       setEvents(res.data.events)
       setTags(res.data.tags)
   })
-
-  console.log("user",user)
-  
-},[])
-
-const newAttendee = (id_event: number) => {
-  axios.post("http://localhost:3001/api/events/attendee",{
-    id_event: id_event,
-    id_user: user.id_user
-  })
-  .then((res: any) => {
-    console.log("res",res.data)
-    
-  }
-  )
-}
-
-const deleteAttendee = (id_event: number) => {
-  axios.delete("http://localhost:3001/api/events/attendee",{
-    data: {
-      id_event: id_event,
-      id_user: user.id_user
-    }  
-  })
-  .then((res: any) => {
-    console.log("res",res.data)
-    
-  }
-  )
-}
-
-
+  },[])
 
   return (
     <>
-      <Typography variant="h3">Events Page</Typography>
-      <Stack>
-          {events.map((elm:Event, key:number)=>{
+    <Typography variant="h3">History</Typography>
+    <Stack>
+        {events.map((elm:Event, key:number)=>{
 
           const eventDay = new Date(elm.date_event_start)
           const today = new Date()
@@ -86,9 +49,11 @@ const deleteAttendee = (id_event: number) => {
           const eventDaySum = 10000*eventDay.getFullYear()+100*eventDay.getMonth()+eventDay.getDate()
           const todaySum = 10000*today.getFullYear()+100*today.getMonth()+today.getDate()
 
-          if(todaySum <= eventDaySum){
-            return (
+          if(eventDaySum < todaySum){
+
+          return (
             <div key={key} style={{border:"1px solid grey", margin:"5px"}}>
+              <div><b>ID: </b>{elm.id_event}</div>
               <div><b>Name: </b>{elm.name_event}</div>
               <div><b>Description: </b>{elm.description_event}</div>
               <div><b>Start: </b>{elm.date_event_start}</div>
@@ -96,22 +61,19 @@ const deleteAttendee = (id_event: number) => {
               <div><b>Location: </b>{elm.location_event}</div>
               <div><b>Category: </b>{elm.category_event}</div>
               <div><b>Price: </b>{elm.price_event}</div>
-              <div><b>ID: </b>{elm.id_event}</div>
               <div><b>Owner: </b>{elm.id_owner}</div>
               <div><b>Tags: </b>
 
-              {tags.map((tag:Tag, key:number)=>{
-                return elm.id_event==tag.id_event? <span key={key}>{tag.name_tag},&nbsp;</span> : null;
-              })}
+                {tags.map((tag:Tag, key:number)=>{
+                  return elm.id_event==tag.id_event? <span key={key}>{tag.name_tag},&nbsp;</span> : null;
+                })}
               
               </div>
-              <button onClick={() => newAttendee(elm["id_event"])}>New Attendee</button>
-              <button onClick={() => deleteAttendee(elm["id_event"])}>Delete Attendee</button>
               
             </div>
-            )}
-          })}
-      </Stack>
-    </>
+          )}
+        })}
+    </Stack>
+  </>
   )
 }
