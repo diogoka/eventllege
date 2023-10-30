621253880c7a75cb87127bfe245283ffcdfa39c4
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-10-29 22:38:10 -0700

    updating branch with main

--- backend/migrations/1697774560049_mvp-migration.js
+++ backend/migrations/1697774560049_mvp-migration.js
@@ -34,7 +34,21 @@ exports.up = async (pgm) => {
     avatar_user: { type: 'bytea', notNull: false },
   });
 
-<<<<<<< HEAD
+  pgm.createTable(tableNameUsers, {
+    id_user: 'varchar(100) primary key',
+    id_user_type: {
+      type: 'integer',
+      references: `${tableNameUsersType}(id_user_type)`,
+      notNull: true,
+      onDelete: 'CASCADE'
+    },
+    name_user: { type: 'varchar(500)', notNull: true },
+    email_user: { type: 'varchar(500)', notNull: true },
+    postal_code_user: { type: 'varchar(500)', notNull: false },
+    phone_user: { type: 'varchar(500)', notNull: false },
+    avatar_user: { type: 'bytea', notNull: false }
+  });
+
   pgm.createTable(courses, {
     id_course: 'serial primary key',
     name_course: { type: 'varchar(500)', notNull: true },
@@ -47,13 +61,13 @@ exports.up = async (pgm) => {
       type: 'VARCHAR(100)',
       references: `${tableNameUsers}(id_user)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
     id_course: {
       type: 'integer',
       references: `${courses}(id_course)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
   });
 
@@ -63,7 +77,7 @@ exports.up = async (pgm) => {
       type: 'VARCHAR(100)',
       references: `${tableNameUsers}(id_user)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
     name_event: { type: 'varchar(500)', notNull: true },
     description_event: { type: 'varchar(500)', notNull: true },
@@ -73,7 +87,7 @@ exports.up = async (pgm) => {
     capacity_event: { type: 'integer', notNull: true },
     price_event: { type: 'integer', notNull: true },
     image_event: { type: 'bytea', notNull: false },
-    category_event: { type: 'varchar(500)', notNull: true },
+    category_event: { type: 'varchar(500)', notNull: true }
   });
 
   pgm.createTable(tags, {
@@ -87,13 +101,13 @@ exports.up = async (pgm) => {
       type: 'integer',
       references: `${events}(id_event)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
     id_tag: {
       type: 'integer',
       references: `${tags}(id_tag)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
   });
 
@@ -102,7 +116,7 @@ exports.up = async (pgm) => {
     id_user: {
       type: 'VARCHAR(100)',
       references: `${tableNameUsers}(id_user)`,
-      notNull: true,
+      notNull: true
     },
     description_review: { type: 'varchar(500)', notNull: true },
     rating: { type: 'integer', notNull: true },
@@ -115,13 +129,13 @@ exports.up = async (pgm) => {
       type: 'integer',
       references: `${events}(id_event)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
     id_review: {
       type: 'integer',
       references: `${reviews}(id_review)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
   });
 
@@ -131,138 +145,16 @@ exports.up = async (pgm) => {
       type: 'VARCHAR(100)',
       references: `${tableNameUsers}(id_user)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
     id_event: {
       type: 'integer',
       references: `${events}(id_event)`,
       notNull: true,
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     },
   });
-=======
-    pgm.createTable(tableNameUsers, {
-        id_user: 'varchar(100) primary key',
-        id_user_type: {
-            type: 'integer',
-            references: `${tableNameUsersType}(id_user_type)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-        name_user: { type: 'varchar(500)', notNull: true },
-        email_user: { type: 'varchar(500)', notNull: true },
-        postal_code_user: { type: 'varchar(500)', notNull: false },
-        phone_user: { type: 'varchar(500)', notNull: false },
-        avatar_user: { type: 'bytea', notNull: false }
-    });
 
-    pgm.createTable(courses, {
-        id_course: 'serial primary key',
-        name_course: { type: 'varchar(500)', notNull: true },
-        category_course: { type: 'varchar(500)', notNull: true },
-    });
-
-    pgm.createTable(users_courses, {
-        id_user_course: 'serial primary key',
-        id_user: {
-            type: 'VARCHAR(100)',
-            references: `${tableNameUsers}(id_user)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-        id_course: {
-            type: 'integer',
-            references: `${courses}(id_course)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-    });
-
-    pgm.createTable(events, {
-        id_event: 'serial primary key',
-        id_owner: {
-            type: 'VARCHAR(100)',
-            references: `${tableNameUsers}(id_user)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-        name_event: { type: 'varchar(500)', notNull: true },
-        description_event: { type: 'varchar(500)', notNull: true },
-        date_event_start: { type: 'timestamp', notNull: true },
-        date_event_end: { type: 'timestamp', notNull: true },
-        location_event: { type: 'varchar(500)', notNull: true },
-        capacity_event: { type: 'integer', notNull: true },
-        price_event: { type: 'integer', notNull: true },
-        image_event: { type: 'bytea', notNull: false },
-        category_event: { type: 'varchar(500)', notNull: true }
-    });
-
-    pgm.createTable(tags, {
-        id_tag: 'serial primary key',
-        name_tag: { type: 'varchar(500)', notNull: true },
-    });
-
-    pgm.createTable(events_tags, {
-        id_event_tag: 'serial primary key',
-        id_event: {
-            type: 'integer',
-            references: `${events}(id_event)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-        id_tag: {
-            type: 'integer',
-            references: `${tags}(id_tag)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-    });
-
-    pgm.createTable(reviews, {
-        id_review: 'serial primary key',
-        id_user: {
-            type: 'VARCHAR(100)',
-            references: `${tableNameUsers}(id_user)`,
-            notNull: true
-        },
-        description_review: { type: 'varchar(500)', notNull: true },
-        rating: { type: 'integer', notNull: true },
-        date_review: { type: 'timestamp', notNull: true },
-    });
-
-    pgm.createTable(events_reviews, {
-        id_event_review: 'serial primary key',
-        id_event: {
-            type: 'integer',
-            references: `${events}(id_event)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-        id_review: {
-            type: 'integer',
-            references: `${reviews}(id_review)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-    });
-
-    pgm.createTable(attendees, {
-        id_attendee: 'serial primary key',
-        id_user: {
-            type: 'VARCHAR(100)',
-            references: `${tableNameUsers}(id_user)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-        id_event: {
-            type: 'integer',
-            references: `${events}(id_event)`,
-            notNull: true,
-            onDelete: 'CASCADE'
-        },
-    });
-
->>>>>>> origin/10-make-the-route-to-delete-an-event
 };
 
 exports.down = async (pgm) => {
