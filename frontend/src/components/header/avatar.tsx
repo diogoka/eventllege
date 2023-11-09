68f51316aee39d5070fc2a4a80230620da04b02d
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-11-09 11:47:01 -0800

    avatar btn  is done

--- frontend/src/components/header/avatar.tsx
+++ frontend/src/components/header/avatar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { UserContext } from '@/context/userContext';
 import { Box } from '@mui/system';
 import { Avatar, IconButton, Drawer } from '@mui/material';
 
@@ -7,15 +8,16 @@ import Hamburger from './hamburger';
 
 export default function AvatarIcon() {
   const [isMenuOpen, setMenuOpen] = useState(false);
-
+  const { user } = useContext(UserContext);
   const toggleMenu = (isMenuOpen: boolean) => {
     setMenuOpen(isMenuOpen);
   };
+  console.log(user?.name);
 
   return (
     <Box sx={{ bgcolor: 'red', position: 'relative', right: '0.8125rem' }}>
       <IconButton edge='start' onClick={() => toggleMenu(true)} sx={{ m: 0 }}>
-        <Avatar>H</Avatar>
+        <Avatar alt={user?.name} src={`http://localhost:3001/img/users/${user?.id}`}></Avatar>
       </IconButton>
       <Drawer anchor='right' open={isMenuOpen} onClose={() => toggleMenu(false)}>
         <Hamburger toggleMenu={toggleMenu} />
