c8de9e49f1b93b4760ecc2314b48fd687af00329
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-11-11 19:50:54 -0800

    set Category componet is done

--- frontend/src/components/events/newEvents/detail-info/capacity.tsx
+++ frontend/src/components/events/newEvents/detail-info/capacity.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { TextField } from '@mui/material';
+import { TextField, InputAdornment } from '@mui/material';
 
 export default function Capacity() {
+  const [spots, setSpots] = React.useState('0');
   return (
     <>
-      <TextField label='Max spots' variant='outlined' type='number' fullWidth />
+      <TextField
+        label='Max spots'
+        variant='outlined'
+        type='number'
+        fullWidth
+        value={spots}
+        onChange={(event) => setSpots(event.target.value)}
+        InputProps={{
+          endAdornment: <InputAdornment position='end'>{spots >= '2' ? 'people' : 'person'}</InputAdornment>,
+        }}
+      />
     </>
   );
 }
