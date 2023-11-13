0aa92eb92753f27f498acae6074148825ed4c832
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-11-13 12:02:09 -0800

    add come label in the page title

--- frontend/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { redirect, RedirectType } from 'next/navigation';
-
-export default function Home() {
-  redirect('/events', RedirectType.replace);
-}
--- frontend/src/components/events/newEvents/detail-info/capacity.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Capacity() {
-  return <div>capacity</div>;
-}
