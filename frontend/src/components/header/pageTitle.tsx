5dfe73dbbb97a20fe44b085a20ec9954e6c78327
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-11-13 13:06:09 -0800

    update currentPages' object

--- frontend/src/components/header/pageTitle.tsx
+++ frontend/src/components/header/pageTitle.tsx
@@ -5,10 +5,11 @@ import { Typography } from '@mui/material';
 const currentPages = [
   { label: 'Home', path: '/events' },
   { label: 'Sign up', path: '/signup' },
+  { label: 'My events', path: '/user/my-events' },
   { label: 'History', path: '/history' },
   { label: 'My profile', path: '/user' },
-  { label: 'created Event', path: '/organizer-events' },
-  { label: 'create Event', path: '/events/new' },
+  { label: 'Created Event', path: '/organizer-events' },
+  { label: 'Create Event', path: '/events/new' },
 ];
 
 export default function PageTitle() {
