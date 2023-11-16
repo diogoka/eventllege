f9cb8c1ef4c077f076f31f14d5d89c2288b78bd8
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-11-15 19:30:07 -0800

    set state in the basciInfo component

--- frontend/src/components/events/newEvents/eventsControl.tsx
+++ frontend/src/components/events/newEvents/eventsControl.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState, useContext } from 'react';
+import { EventContext } from '@/context/eventContext';
 import { Box } from '@mui/material';
 import BasicInfo from './basic-info/basicInfo';
 import DateList from './dateSchedule/dateList';
@@ -7,9 +9,12 @@ import Location from './location/location';
 import ImageContainer from '../newEvents/basic-info/imageContainer';
 
 export default function EventsControl() {
+  const { eventData, setEventData } = useContext(EventContext);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
   return (
     <Box component={'form'}>
-      <BasicInfo />
+      <BasicInfo title={title} setTitle={setTitle} description={description} setDescription={setDescription} />
       <DateList />
       <Location />
       <DetailList />
--- frontend/src/context/eventContext.tsx
+++ frontend/src/context/eventContext.tsx
@@ -1,36 +1,35 @@
-'use client'
+'use client';
 import React, { createContext, ReactNode, useState } from 'react';
 
-  type EventData = {
-    title: string;
-    description: string;
-    dates: Array<Date>;
-    spots: string;
-    location: string;
-    price: string;
-    image: string;
-    tagId: number;
-    category: string;
-    tags: Array<number>;
-    categories: Array<string>;
-  }
+export type EventData = {
+  title: string;
+  description: string;
+  dates: Array<Date>;
+  spots: string;
+  location: string;
+  price: string;
+  image: string;
+  tagId: number;
+  category: string;
+  tags: Array<number>;
+  categories: Array<string>;
+};
 
 type EventContextProps = {
-  eventData: EventData | null,
-  setEventData: (EventData: EventData | null) => void
-}
+  eventData: EventData | null;
+  setEventData: (EventData: EventData | null) => void;
+  // setEventData: React.Dispatch<React.SetStateAction<EventData | null>>;
+  // updateProperty: (property: keyof EventData, value: string) => void;
+};
 
 export const EventContext = createContext<EventContextProps>({} as EventContextProps);
 
 export function EventContextProvider({ children }: { children: ReactNode }) {
-  
   const [eventData, setEventData] = useState<EventData | null>(null);
 
-  return (
-    <EventContext.Provider
-      value={{ eventData, setEventData }}
-    >
-      {children}
-    </EventContext.Provider>
-  );
+  const updateProperty = (property: keyof EventData, value: string) => {
+    setEventData((prev) => (prev ? { ...prev, [property]: value } : null));
+  };
+
+  return <EventContext.Provider value={{ eventData, setEventData }}>{children}</EventContext.Provider>;
 }
