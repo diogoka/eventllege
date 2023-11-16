ac8a480286b931222525e8cf0fb98046faaec777
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-11-16 11:15:15 -0800

    added the message when there are not events

--- frontend/src/app/organizer-events/page.tsx
+++ frontend/src/app/organizer-events/page.tsx
@@ -1,51 +1,49 @@
 'use client';
-import { useEffect, useState, useContext } from 'react'
-import { Box } from '@mui/material'
+import { useEffect, useState, useContext } from 'react';
+import { Box } from '@mui/material';
 import Alert from '@mui/material/Alert';
-import axios from 'axios'
-import EventList from '@/components/events/eventList'
+import axios from 'axios';
+import EventList from '@/components/events/eventList';
 import SearchBar from '@/components/searchBar';
 import { UserContext } from '@/context/userContext';
 
-
 type Event = {
-  id_event: number,
-  id_owner: string,
-  name_event: string,
-  description_event: string,
-  date_event_start: string,
-  date_event_end: string,
-  location_event: string,
-  capacity_event: number,
-  price_event: number,
-  image_event: string,
-  category_event: string
-}
+  id_event: number;
+  id_owner: string;
+  name_event: string;
+  description_event: string;
+  date_event_start: string;
+  date_event_end: string;
+  location_event: string;
+  capacity_event: number;
+  price_event: number;
+  image_event: string;
+  category_event: string;
+};
 
 type Tag = {
-  id_event: number,
-  name_tag: string
-}
+  id_event: number;
+  name_tag: string;
+};
 
 type CurrentUser = {
   id: string;
   role: string;
-}
+};
 
 export default function OrganizerEventsPage() {
-
-
   const { user } = useContext(UserContext);
   const [events, setEvents] = useState<Array<Event>>([]);
   const [tags, setTags] = useState<Array<Tag>>([]);
   const [alertOpen, setAlertOpen] = useState(false);
-
-
+  const [eventsOfUser, setEventsOfUser] = useState<Array<[number, boolean]>>(
+    []
+  );
 
   const currentUser: CurrentUser = {
     id: user!.id,
     role: user!.roleName,
-  }
+  };
 
   useEffect(() => {
     axios
@@ -56,15 +54,32 @@ export default function OrganizerEventsPage() {
       })
       .catch((error) => {
         console.error(error.response.data);
-      })
+      });
+    const attendingEvents: [number, boolean][] = [];
+    axios
+      .get(`http://localhost:3001/api/events/user/${currentUser.id}`)
+      .then((res) => {
+        res.data.events.map((event: Event) => {
+          let attendingEvent: [number, boolean] = [event.id_event, true];
+          attendingEvents.push(attendingEvent);
+        });
+      });
+    setEventsOfUser(attendingEvents);
   }, []);
 
   const searchEvents = (text: string) => {
     console.log(text);
-  }
+  };
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
       {alertOpen && (
         <Alert
           severity="info"
@@ -76,8 +91,13 @@ export default function OrganizerEventsPage() {
         </Alert>
       )}
       <SearchBar searchEvents={searchEvents} />
-      <EventList events={events} tags={tags} user={currentUser} setEvents={setEvents}></EventList>
+      <EventList
+        events={events}
+        tags={tags}
+        user={currentUser}
+        setEvents={setEvents}
+        attendance={eventsOfUser}
+      ></EventList>
     </Box>
-  )
-
+  );
 }
--- frontend/src/components/searchBar.tsx
+++ frontend/src/components/searchBar.tsx
@@ -10,10 +10,9 @@ import Box from '@mui/material/Box';
 
 type Props = {
   searchEvents: (text: string) => void;
-  
 };
 
-function SearchBar({ searchEvents }:Props) {
+function SearchBar({ searchEvents }: Props) {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = (event: any) => {
@@ -31,7 +30,7 @@ function SearchBar({ searchEvents }:Props) {
     marginBottom: '1rem',
     height: '3rem',
   };
-  
+
   const textFieldStyle = {
     border: '0px solid #141D4F',
     borderRadius: '0px',
@@ -62,7 +61,7 @@ function SearchBar({ searchEvents }:Props) {
 
   return (
     <Grid container spacing={0} style={gridContainerStyle}>
-      <Grid item xs={9.5} sx={{height: '3rem'}}>
+      <Grid item xs={9.5} sx={{ height: '3rem' }}>
         <TextField
           value={searchTerm}
           onChange={handleInputChange}
@@ -74,7 +73,13 @@ function SearchBar({ searchEvents }:Props) {
       </Grid>
       <Grid item xs={2.5} style={gridIconContainerStyle}>
         <Box>
-          <IconItem iconName="FaSearch" onClick={handleSearch} isClickable={true} color='white' size="1.3rem" />
+          <IconItem
+            iconName="FaSearch"
+            onClick={handleSearch}
+            isClickable={true}
+            color="white"
+            size="1.3rem"
+          />
         </Box>
       </Grid>
     </Grid>
