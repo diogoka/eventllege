27974b0050016da605d3a5d97ec5debbea469db6
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-11-18 20:10:19 -0800

    create responsive switch contorol that is Swhicher

---
+++ frontend/src/components/header/headerPC.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from './logo';
+
+export default function HeaderPC() {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+}
--- frontend/src/components/header/logo.tsx
+++ frontend/src/components/header/logo.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@mui/material';
+import Switcher from '../common/switcher';
 
 export default function Logo() {
   const router = useRouter();
   return (
-    <Button onClick={() => router.push('/')} sx={{ width: '3.125rem', height: '3.125rem' }}>
-      <img src={'/eventllege_Icon.svg'} alt='eventllege logo' style={{ width: '2.5rem', height: '2.5rem' }} />
-    </Button>
+    <Switcher
+      sp={
+        <Button
+          onClick={() => router.push('/')}
+          sx={{ width: '3.125rem', height: '3.125rem' }}
+        >
+          <img
+            src={'/eventllege_Icon.svg'}
+            alt="eventllege logo"
+            style={{ width: '2.5rem', height: '2.5rem' }}
+          />
+        </Button>
+      }
+      pc={
+        <Button
+          onClick={() => router.push('/')}
+          sx={{ width: '3.125rem', height: '3.125rem' }}
+        >
+          <img
+            src={'/eventllege_logo.svg'}
+            alt="eventllege logo"
+            style={{ width: '2.5rem', height: '2.5rem' }}
+          />
+        </Button>
+      }
+    />
   );
 }
