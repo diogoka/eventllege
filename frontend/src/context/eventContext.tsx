5ad977c7d4b24593231d837fe22e2ce259fd61ff
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-11-21 11:14:55 -0800

    replace useReducer from eventsControl to eventsContext

--- frontend/src/context/eventContext.tsx
+++ frontend/src/context/eventContext.tsx
@@ -1,44 +1,112 @@
 'use client';
-import React, { createContext, ReactNode, useState } from 'react';
-import { DateRange } from '@/components/events/newEvents/eventsControl';
+import React, {
+  createContext,
+  ReactNode,
+  useState,
+  useReducer,
+  Dispatch,
+} from 'react';
+import dayjs from 'dayjs';
+// import { DateRange } from '@/components/events/newEvents/eventsControl';
 
+export interface DateRange {
+  dateStart: dayjs.Dayjs;
+  dateEnd: dayjs.Dayjs;
+}
+const today = dayjs();
 type Tag = {
   id_tag: number;
   name_tag: string;
 };
 export type EventData = {
-  title: string;
-  description: string;
+  name_event: string;
+  description_event: string;
   dates: DateRange[];
-  spots: number;
-  location: string;
-  price: number;
+  capacity_event: number;
+  location_event: string;
+  price_event: number;
   selectedTags: Tag[];
-  category: string;
+  category_event: string;
 };
 
 export type Image = File | null;
 
 type EventContextProps = {
-  eventData: EventData | null;
-  setEventData: (EventData: EventData | null) => void;
+  // eventData: EventData | null;
+  // setEventData: (EventData: EventData | null) => void;
   addImage: Image | undefined;
   setAddImage: (addImage: Image) => void;
+  createdEvent: EventData;
+  dispatch: Dispatch<EventAction>;
 };
 
+type EventAction = {
+  type: string;
+  payload: EventData;
+};
+
+const initialState: EventData = {
+  name_event: '',
+  description_event: '',
+  dates: [{ dateStart: today, dateEnd: today }],
+  capacity_event: -1,
+  location_event: '',
+  price_event: 0,
+  selectedTags: [],
+  category_event: '',
+
+  // title: editEvent ? editEvent.name_event : '',
+  // description: editEvent ? editEvent.description_event : '',
+  // dates: editEvent
+  //   ? [
+  //       {
+  //         dateStart: dayjs(editEvent.date_event_start),
+  //         dateEnd: dayjs(editEvent.date_event_end),
+  //       },
+  //     ]
+  //   : [{ dateStart: today, dateEnd: today }],
+  // spots: editEvent ? editEvent.capacity_event : 0,
+  // location: editEvent ? editEvent.location_event : 'location',
+  // price: editEvent ? editEvent.price_event : 0,
+  // image: editEvent ? editEvent.image_event : 'images',
+  // selectedTags: selectedTags ? selectedTags : [],
+  // category: editEvent ? editEvent.category_event : '',
+};
+
+export const eventReducer = (state: EventData, action: EventAction) => {
+  switch (action.type) {
+    case 'UPDATE_TITLE':
+      return { ...state, name_event: action.payload.name_event };
+    case 'UPDATE_DESCRIPTION':
+      return { ...state, description_event: action.payload.description_event };
+    case 'UPDATE_DATES':
+      return { ...state, dates: action.payload.dates };
+    case 'UPDATE_PRICE':
+      return { ...state, price_event: action.payload.price_event };
+    case 'UPDATE_SPOTS':
+      return { ...state, capacity_event: action.payload.capacity_event };
+    case 'UPDATE_CATEGORY':
+      return { ...state, category_event: action.payload.category_event };
+    case 'UPDATE_SELECTED_TAGS':
+      return { ...state, selectedTags: action.payload.selectedTags };
+    default:
+      return state;
+  }
+};
 export const EventContext = createContext<EventContextProps>(
   {} as EventContextProps
 );
 
 export function EventContextProvider({ children }: { children: ReactNode }) {
-  const [eventData, setEventData] = useState<EventData | null>(null);
+  // const [eventData, setEventData] = useState<EventData | null>(null);
   const [addImage, setAddImage] = useState<Image>(null);
-  console.log('eventContext', eventData);
-  console.log('eventContext images', addImage);
+  const [createdEvent, dispatch]: [EventData, Dispatch<EventAction>] =
+    useReducer(eventReducer, initialState);
+  console.log('created event', createdEvent);
 
   return (
     <EventContext.Provider
-      value={{ eventData, setEventData, addImage, setAddImage }}
+      value={{ createdEvent, dispatch, addImage, setAddImage }}
     >
       {children}
     </EventContext.Provider>
