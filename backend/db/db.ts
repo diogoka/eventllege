40a188c24450f4004cee37cf433bc943771663cf
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-12-01 14:23:32 -0800

    deploy

--- backend/db/db.ts
+++ backend/db/db.ts
@@ -1,19 +1,8 @@
-import Pool from 'pg';
-import 'dotenv/config'
+import { Pool } from 'pg';
+import 'dotenv/config';
 
-const host = String(process.env.HOST);
-const user = String(process.env.USER);
-const password = String(process.env.PASSWORD);
-const database = String(process.env.DATABASE);
-const port_db = Number(process.env.PORT_DB);
-
-
-const pool = new Pool.Pool({
-    user: user,
-    host: host,
-    password: password,
-    database: database,
-    port: port_db
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
 export default pool;
