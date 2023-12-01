33eb9918b2e7bb8c160c380c26076de74b371e45
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-12-01 08:03:17 -0800

    wehn update or create btn is clicked, navigation btn is moved

--- backend/routes/tagsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { getAllTags } from "../controllers/tagsControllers";
-
-
-const tagsRouter: Router = express.Router();
-
-tagsRouter.get("/", getAllTags);
-
-export default tagsRouter;
