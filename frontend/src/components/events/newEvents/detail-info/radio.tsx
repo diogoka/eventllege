3e53e539bc2e8ab7d16280804ee740c09f7337a9
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-12-03 10:54:51 -0800

    comment out affected label function

--- frontend/src/components/events/newEvents/detail-info/radio.tsx
+++ frontend/src/components/events/newEvents/detail-info/radio.tsx
@@ -78,32 +78,32 @@ export default function RadioBtn() {
           value={16}
           control={<Radio />}
           label='Online'
-          checked={
-            createdEvent &&
-            createdEvent.selectedTags.some((tag) => tag.id_tag === 16)
-          }
+          // checked={
+          //   createdEvent &&
+          //   createdEvent.selectedTags.some((tag) => tag.id_tag === 16)
+          // }
         />
         <FormControlLabel
           value={17}
           control={<Radio />}
           label='In Person'
-          checked={
-            createdEvent &&
-            createdEvent.selectedTags.some((tag) => tag.id_tag === 17)
-          }
+          // checked={
+          //   createdEvent &&
+          //   createdEvent.selectedTags.some((tag) => tag.id_tag === 17)
+          // }
         />
         <FormControlLabel
           value={18}
           control={<Radio />}
           label='Online and In Person'
-          checked={
-            createdEvent &&
-            createdEvent.selectedTags.some(
-              (tag) =>
-                tag.id_tag === 16 &&
-                createdEvent.selectedTags.some((tag) => tag.id_tag === 17)
-            )
-          }
+          // checked={
+          //   createdEvent &&
+          //   createdEvent.selectedTags.some(
+          //     (tag) =>
+          //       tag.id_tag === 16 &&
+          //       createdEvent.selectedTags.some((tag) => tag.id_tag === 17)
+          //   )
+          // }
         />
       </RadioGroup>
     </FormControl>
