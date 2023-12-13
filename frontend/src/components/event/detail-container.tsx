136dbcb9bca650ac62bf9ad3e7b296404d3a92d9
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-12-13 09:49:27 -0800

    update

---
+++ frontend/src/components/event/detail-container.tsx
@@ -0,0 +1,119 @@
+import { useContext } from 'react';
+import { Box, Typography, Link } from '@mui/material';
+import ImageHelper from '@/components/common/image-helper';
+import {
+  Event,
+  OtherInfo,
+  DetailPageContext,
+} from '../../app/events/[id]/page';
+import IconsContainer from '../icons/iconsContainer';
+import DetailIconContainer from './detail-icon-container';
+import DetailTimeContainer from './detail-time-container';
+import alertFn from '@/components/common/alertFunction';
+
+export type Props = {
+  event: Event;
+  applied: boolean;
+  organizerEvent: boolean;
+  otherInfo: OtherInfo;
+  forMobile: boolean;
+  forPreview: boolean;
+  maxSpots?: number;
+};
+
+const DetailContainer = ({
+  event,
+  otherInfo,
+  applied,
+  organizerEvent,
+  forMobile,
+  forPreview,
+}: Props) => {
+  const { isAlertVisible, setIsAlertVisible } = useContext(DetailPageContext);
+  const locationContainerStyle = {
+    fontSize: forMobile ? 'auto' : '1.2rem',
+    marginTop: '3px',
+  };
+  const h1Style = !forMobile
+    ? { textAlign: 'left', fontWeight: 'bold', fontSize: '2.5rem' }
+    : null;
+  const timeContainerStyle = { margin: forMobile ? '10px auto' : '40px auto' };
+
+  return (
+    <>
+      <Typography variant='h1' sx={h1Style}>
+        {event?.name_event}
+      </Typography>
+
+      <Box
+        display={forMobile ? 'block' : 'none'}
+        style={{ marginInline: 'auto' }}
+      >
+        <ImageHelper
+          src={`http://localhost:3001/img/events/${otherInfo?.id_event}`}
+          width='100%'
+          height='auto'
+          alt={event?.name_event ?? 'Event'}
+        />
+      </Box>
+
+      <Box display={forMobile ? 'block' : 'none'}>
+        <DetailIconContainer
+          event={event!}
+          otherInfo={otherInfo!}
+          applied={applied}
+          organizerEvent={organizerEvent}
+          forMobile={forMobile}
+          forPreview={forPreview}
+        />
+      </Box>
+
+      <Box style={timeContainerStyle}>
+        <DetailTimeContainer
+          event={event!}
+          otherInfo={otherInfo!}
+          applied={applied}
+          organizerEvent={organizerEvent}
+          forMobile={forMobile!}
+        />
+
+        <Link
+          href={`https://maps.google.com/?q=${event?.location_event}`}
+          target='_blank'
+        >
+          <Box display='flex' alignItems='center'>
+            <IconsContainer
+              icons={[
+                { name: 'FaLocationArrow', isClickable: false, color: 'navy' },
+              ]}
+              onIconClick={() => {
+                return;
+              }}
+            />
+            <Typography sx={locationContainerStyle}>
+              {event?.location_event}
+            </Typography>
+          </Box>
+        </Link>
+      </Box>
+
+      <Box
+        fontSize={forMobile ? '1em' : '1.1em'}
+        margin={forMobile ? '10px auto' : '30px auto'}
+      >
+        <Box fontWeight='bold'>About this event:</Box>
+        {event?.description_event}
+      </Box>
+
+      {isAlertVisible &&
+        alertFn(
+          'URL Copied',
+          'The event URL has been copied to your clipboard.',
+          'success',
+          () => setIsAlertVisible(false)
+        )}
+    </>
+  );
+};
+
+export default DetailContainer;
