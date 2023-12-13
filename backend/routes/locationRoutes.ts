4fabda1502b89d71d61f45ae26a0522e32661858
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-12-13 09:48:52 -0800

    fixed navigation of student

--- backend/routes/locationRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from 'express';
-import { getLocation } from '../controllers/locationControllers';
-
-const locationRouter: Router = express.Router();
-
-locationRouter.get('/', getLocation);
-
-export default locationRouter;
--- frontend/src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-import NotFound from '@/components/common/notFound'
-
-
-export default function NotFoundPage() {
-  return (
-    <NotFound />
-  )
-}
