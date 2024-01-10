583467777db446ef55de133ab71fe812caa24831
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2024-01-10 14:04:49 -0800

    Backend ready to deploy

--- backend/app.ts
+++ backend/app.ts
@@ -9,7 +9,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: '*',
   methods: 'GET, POST, PUT, DELETE',
 };
 
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
--- backend/db/db.ts
+++ backend/db/db.ts
@@ -1,18 +1,8 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
 
-const host = String(process.env.HOST);
-const user = String(process.env.USER);
-const password = String(process.env.PASSWORD);
-const database = String(process.env.DATABASE);
-const port_db = Number(process.env.PORT_DB);
-
 const pool = new Pool({
-  user: user,
-  host: host,
-  password: password,
-  database: database,
-  port: port_db,
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
 export default pool;
